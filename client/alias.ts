07e69d861bd4ec9a979d060ba602f4e45bba2ca7
ToyboxZach <zach@make.toys>
2025-05-22 07:57:08 -0700

    fix example

--- client/alias.ts
+++ client/alias.ts
@@ -1,1 +1,2 @@
 console.log("LOADED ALIAS");
+export default {};
--- client/main.tsx
+++ client/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Meteor } from "meteor/meteor";
 import { App } from "/imports/ui/App";
-import "@alias/file";
+import _default from "@alias/file";
+console.log("LOADED ALIAS", _default);
 Meteor.startup(() => {
   const container = document.getElementById("react-target");
   const root = createRoot(container!);

ab0ab6d0d86f5a6eb712b4b390f0539dab9627b8
ToyboxZach <zach@make.toys>
2025-05-28 08:58:02 -0700

    Fix up test

--- client/main.tsx
+++ client/main.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Meteor } from "meteor/meteor";
 import { App } from "/imports/ui/App";
-import _default from "@alias/file";
-console.log("LOADED ALIAS", _default);
+import "@alias/file";
 Meteor.startup(() => {
   const container = document.getElementById("react-target");
   const root = createRoot(container!);
--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { Hello } from './Hello';
-import { Info } from './Info';
+import React from "react";
+import { Hello } from "./Hello";
+import { Info } from "./Info";
 
+import { Svg, Line } from "react-native-svg";
+import { getCountry } from "react-native-localize";
+
+console.log("Country:", getCountry());
 export const App = () => (
   <div>
+    <Svg pointerEvents="none" width={20} height={10} viewBox={`0 0 ${5} ${5}`}>
+      <Line x1={5} y1={0} x2={10} y2={20} strokeLinecap="round" strokeWidth={3} />
+      <Line x1={5} y1={10} x2={10} y2={20} strokeLinecap="round" strokeWidth={3} />
+    </Svg>
     <h1>Welcome to Meteor!</h1>
     <Hello />
     <Info />

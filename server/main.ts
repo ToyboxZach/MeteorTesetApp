1c81299c3ef2f316bca5ace27c2cdafee6e77cbb
ToyboxZach <zach@make.toys>
2025-09-09 11:15:53 -0700

    New bug

--- server/main.ts
+++ server/main.ts
@@ -1,3 +1,5 @@
 import { S3Client } from "@aws-sdk/client-s3";
 console.log("s3client", S3Client);
 import "meteor/aldeed:collection2/static";
+import { bcrypt } from "bcrypt";
+console.log("bcrypt", bcrypt);

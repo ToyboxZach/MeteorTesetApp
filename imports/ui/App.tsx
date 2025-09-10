053f66ad38e8e0becaed35a1bd21fa7e494d3393
ToyboxZach <zach@make.toys>
2025-09-10 11:54:13 -0700

    shopify not working

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Hello } from './Hello';
-import { Info } from './Info';
+import React from "react";
+import { Info } from "./Info";
+import { NonComplying } from "./nonComplying";
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello />
+    <NonComplying />
     <Info />
   </div>
 );
--- server/main.ts
+++ server/main.ts
@@ -4,3 +4,16 @@ console.log("s3client", S3Client);
 import "meteor/aldeed:collection2/static";
 import { bcrypt } from "bcrypt";
 console.log("bcrypt", bcrypt);
+import { ApiVersion, shopifyApi } from "@shopify/shopify-api";
+import "@shopify/shopify-api/dist/cjs/adapters/node";
+
+shopifyApi({
+  apiKey: "apiKey",
+  apiSecretKey: "secret",
+  hostName: "host",
+  scopes: ["write_orders"],
+  isEmbeddedApp: true,
+  apiVersion: ApiVersion.July25
+
+  // SESSION_STORAGE: new Shopify.Session.CustomSessionStorage(StoreSession, GetSession, DeleteSession),
+});

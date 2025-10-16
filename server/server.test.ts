e433315c887ceca1565d5494ba4ab038cd999665
ToyboxZach <zach@make.toys>
2025-10-16 14:59:16 -0700

    Add files

---
+++ server/server.test.ts
@@ -0,0 +1,9 @@
+import assert from "assert";
+import { Meteor } from "meteor/meteor";
+
+describe("run server test", () => {
+  it("Runs a server test", async () => {
+    console.log("Server test ran");
+    assert(Meteor.isServer);
+  });
+});

a28c31a50d6bdf2c4aef09ce3ed12913ab141d79
ToyboxZach <zach@make.toys>
2025-11-25 10:18:38 -0800

    Tests pass on meteor 3.3

--- client/main.tsx
+++ client/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Meteor } from "meteor/meteor";
 import { App } from "/imports/ui/App";
-import _default from "@alias/file";
-console.log("LOADED ALIAS", _default);
 Meteor.startup(() => {
   const container = document.getElementById("react-target");
   const root = createRoot(container!);
